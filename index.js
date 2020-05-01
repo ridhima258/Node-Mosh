9d43f3f4ec510b10c0aaddff16cadd74f0bea4f6
Ridhima Kapoor <ridhima.1610127@kiet.edu>
2020-05-02 01:17:25 +0530

    Enter

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const debug = require('debug')('app:startup');
 const config=require('config');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const Joi = require('joi');
 const express=require('express');
 const app=express();
-
+const courses=require('./routes/courses');
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.static('public'));
 app.use(helmet());
+app.use('/api/course',courses);
 
 //Configuration
 console.log('Application Name:'+config.get('name'));
@@ -17,77 +19,13 @@ console.log('Mail password:'+config.get('mail.password'));
 if(app.get('env') === 'development')
 {
     app.use(morgan('tiny'));
-    console.log('Morgan enabled');
+    debug('Morgan enabled');
 }
 
-const courses=[ 
-{id:1, name:'course1'},
-{id:2, name:'course2'},
-{id:3, name:'course3'}
-];
+
 app.get('/',(req,res)=>{
     res.send('Hello World!!!!');
 });
-
-app.get('/api/courses',(req,res)=>{
-    res.send(courses);
-});
-
-app.get('/api/courses/:id',(req,res)=>{
-    const course=courses.find(c=>c.id===parseInt(req.params.id));
-    if(!course) return res.status(404).send('Given id is not found');
-    res.send(course);
-}); 
-
-app.post('/api/courses', (req,res) => {
-    const { error } = validateCourse(req.body) ;
-    if(error) return res.status(400).send({data: null, message: result.error.details[0].message, error:true});
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    return res.send({data: course, message: "course added", error: false});
-});
-
-
-
-app.put('/api/courses/:id', (req,res)=>{
-    //Check if course exists
-    const course=courses.find(c=>c.id===parseInt(req.params.id));
-    if(!course) return res.status(404).send('Given id is not found');
-    
-    //validate the request
-    const { error }=validateCourse(req.body);
-    if(error) return res.status(400).send({data: null, message: result.error.details[0].message, error:true});
-
-    //update the course
-    course.name=req.body.name;
-    res.send(course);
-});
-
-
-app.delete('/api/courses/:id',(req,res)=>{
-    const course=courses.find(c=>c.id===parseInt(req.params.id));
-    if(!course) return res.status(404).send('Given id is not found');
-    
-    const index=courses.indexOf(course);
-    courses.splice(index,1);
-
-    res.send(course);
-});
-
-
-
-function validateCourse(course)
-{
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(course, schema) ;
-}
-
-
 
 
 //PORT
---
+++ routes/courses.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const router = express.Router();
+
+const courses=[ 
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'}
+    ];
+
+router.get('/',(req,res)=>{
+    res.send(courses);
+});
+
+router.get('/:id',(req,res)=>{
+    const course=courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course) return res.status(404).send('Given id is not found');
+    res.send(course);
+}); 
+
+router.post('/', (req,res) => {
+    const { error } = validateCourse(req.body) ;
+    if(error) return res.status(400).send({data: null, message: result.error.details[0].message, error:true});
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    return res.send({data: course, message: "course added", error: false});
+});
+
+
+
+router.put('/:id', (req,res)=>{
+    //Check if course exists
+    const course=courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course) return res.status(404).send('Given id is not found');
+    
+    //validate the request
+    const { error }=validateCourse(req.body);
+    if(error) return res.status(400).send({data: null, message: result.error.details[0].message, error:true});
+
+    //update the course
+    course.name=req.body.name;
+    res.send(course);
+});
+
+
+router.delete('/:id',(req,res)=>{
+    const course=courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course) return res.status(404).send('Given id is not found');
+    
+    const index=courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+});
+
+
+
+function validateCourse(course)
+{
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema) ;
+}
+
+module.exports=router;
